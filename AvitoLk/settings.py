8aeeee119a7f49027271ceb4071d617d82b070b7
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-04-21 21:22:03 +0300

    some-settings

--- AvitoLk/settings.py
+++ AvitoLk/settings.py
@@ -81,67 +81,67 @@ WSGI_APPLICATION = 'AvitoLk.wsgi.application'
 # Logging
 
 
-FORMATTERS = (
-    {
-        'verbose': {
-            'format': '{levelname} {asctime:s} {threadName} {thread:d} {module} {filename} {lineno:d} {name} {funcName} {process:d} {message}',
-            'style': '{',
-        },
-        'simple': {
-            'format': '{levelname} {asctime:s} {module} {filename} {lineno:d} {funcName} {message}',
-            'style': '{',
-        },
-    },
-)
-
-HANDLERS = (
-    {
-        'console_handler': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-        },
-        'my_handler': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': f'{BASE_DIR}/logs/form_parsing_log.log',
-            'mode': 'a',
-            'encoding': 'utf-8',
-            'formatter': 'simple',
-            'backupCount': 5,
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-        },
-        'my_handler_detailed': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': f'{BASE_DIR}/logs/form_parsing_log_detailed.log',
-            'mode': 'a',
-            'formatter': 'verbose',
-            'backupCount': 5,
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-        },
-    },
-)
-
-LOGGERS = (
-    {
-        'django': {
-            'handlers': ["console_handler", "my_handler_detailed"],
-            'level': getenv('DJANGO_LOG_LEVEL', 'INFO'),
-            'propogate': False,
-        },
-        'django.request': {
-            'handlers': ['my_handler'],
-            'level': getenv('DJANGO_REQUEST_LOG_LEVEL', 'WARNING'),
-            'propogate': False,
-        },
-    },
-)
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': FORMATTERS[0],
-    'handlers': HANDLERS[0],
-    'loggers': LOGGERS[0],
-}
+# FORMATTERS = (
+#     {
+#         'verbose': {
+#             'format': '{levelname} {asctime:s} {threadName} {thread:d} {module} {filename} {lineno:d} {name} {funcName} {process:d} {message}',
+#             'style': '{',
+#         },
+#         'simple': {
+#             'format': '{levelname} {asctime:s} {module} {filename} {lineno:d} {funcName} {message}',
+#             'style': '{',
+#         },
+#     },
+# )
+#
+# HANDLERS = (
+#     {
+#         'console_handler': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple',
+#         },
+#         'my_handler': {
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'filename': f'{BASE_DIR}/logs/form_parsing_log.log',
+#             'mode': 'a',
+#             'encoding': 'utf-8',
+#             'formatter': 'simple',
+#             'backupCount': 5,
+#             'maxBytes': 1024 * 1024 * 5,  # 5 MB
+#         },
+#         'my_handler_detailed': {
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'filename': f'{BASE_DIR}/logs/form_parsing_log_detailed.log',
+#             'mode': 'a',
+#             'formatter': 'verbose',
+#             'backupCount': 5,
+#             'maxBytes': 1024 * 1024 * 5,  # 5 MB
+#         },
+#     },
+# )
+#
+# LOGGERS = (
+#     {
+#         'django': {
+#             'handlers': ["console_handler", "my_handler_detailed"],
+#             'level': getenv('DJANGO_LOG_LEVEL', 'INFO'),
+#             'propogate': False,
+#         },
+#         'django.request': {
+#             'handlers': ['my_handler'],
+#             'level': getenv('DJANGO_REQUEST_LOG_LEVEL', 'WARNING'),
+#             'propogate': False,
+#         },
+#     },
+# )
+#
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': FORMATTERS[0],
+#     'handlers': HANDLERS[0],
+#     'loggers': LOGGERS[0],
+# }
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
