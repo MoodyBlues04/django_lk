969d6d41362cf0b2dff7d28b99bd25f1a46826f5
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-04-23 17:20:26 +0300

    images-for-active-projects-set-up

--- AvitoLk/settings.py
+++ AvitoLk/settings.py
@@ -196,6 +196,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "AvitoLk/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
