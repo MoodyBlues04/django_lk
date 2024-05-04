cafee9e74d050cb2e8e685691f299342db483d27
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-05-05 00:02:23 +0300

    parallel-exec

--- base/management/commands/randomize_projects.py
+++ base/management/commands/randomize_projects.py
@@ -1,19 +1,35 @@
-import time
+import django
+
+django.setup()
 
 from django.core.management.base import BaseCommand, CommandError
 from base.models import User, Project
 from base.helpers.sheet_randomizer import SheetRandomizer
+import multiprocessing as mp
+
+randomizer = SheetRandomizer()
+
+
+def process_user(user):
+    try:
+        print(f"User: {user.username}")
+        projects = Project.objects.filter(user=user.id)
+        for project_idx, project in enumerate(projects):
+            print(f"\tProject: {project_idx}. {project.name}")
+            randomizer.update_sheet(project.sheet_id)
+    except Exception as e:
+        print('Error occured: ' + str(e))
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        randomizer = SheetRandomizer()
         users = User.objects.filter(role=User.Role.ROLE_CUSTOMER)
-        for user_idx, user in enumerate(users):
-            print(f"User: {user_idx}. {user.username}")
 
-            projects = Project.objects.filter(user=user.id)
-            for project_idx, project in enumerate(projects):
-                print(f"\tProject: {project_idx}. {project.name}")
-                randomizer.update_sheet(project.sheet_id)
-                time.sleep(5)
+        self.run_in_parallel([process_user] * len(users), users)
+
+    def run_in_parallel(self, tasks: list, args: list):
+        processes = [mp.Process(target=_task, args=(args[task_idx],)) for task_idx, _task in enumerate(tasks)]
+        for running_task in processes:
+            running_task.start()
+        for running_task in processes:
+            running_task.join()
